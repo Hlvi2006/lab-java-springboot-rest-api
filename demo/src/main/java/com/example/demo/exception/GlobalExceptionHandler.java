048687b4ca6c1b1cc004b03f1c54e59dda379021
Ulvi Haciyev <ulvihaciyev551@gmail.com>
2026-03-15 21:08:02 +0400

    Product test added

--- demo/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ demo/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -6,42 +6,49 @@ import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handelResourceNotFoundException(ResourceNotFoundException ex){
+        ErrorResponse errorResponse=new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                ex.getMessage(),
+                "Not found"
+
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String,String>> handleValidationException(MethodArgumentNotValidException ex){
+    public ResponseEntity<ErrorResponse> handelIllegalArgumentException(MethodArgumentNotValidException ex){
+        List<String> errors=new ArrayList<>();
 
-        Map<String,String> errors = new HashMap<>();
+        for(var error:ex.getBindingResult().getFieldErrors()){
+            errors.add(error.getField()+" "+error.getDefaultMessage());
+        }
 
-        ex.getBindingResult().getFieldErrors()
-                .forEach(error ->
-                        errors.put(error.getField(), error.getDefaultMessage())
-                );
+        ErrorResponse errorResponse=new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "Validation failed",
+                "Bad request",
+                errors
 
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
-    // Missing API-Key header
-    @ExceptionHandler(MissingRequestHeaderException.class)
-    public ResponseEntity<String> handleMissingHeader(MissingRequestHeaderException ex){
-
-        return new ResponseEntity<>("API-Key header is missing", HttpStatus.BAD_REQUEST);
-    }
-
-
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handleProductNotFound(RuntimeException ex){
-
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
-    }
-
-
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleInvalidPrice(IllegalArgumentException ex){
-
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handelException(Exception ex){
+        ErrorResponse errorResponse=new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Validation failed",
+                "Internal server error"
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
     }
 }
+
